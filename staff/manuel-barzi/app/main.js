8a515288fdb89bba288426ac0ba146156dce5f09
manuelbarzi <manuelbarzi@gmail.com>
2023-04-24 16:35:11 +0200

    separate the concerns (business logic from presentation); add create post stories, screens, and presentation logic

--- staff/manuel-barzi/app/main.js
+++ staff/manuel-barzi/app/main.js
@@ -28,14 +28,29 @@ user.password = password
 
 users.push(user)
 
-// presentation
+var posts = []
 
-document.querySelector('.login-page').querySelector('form').onsubmit = function (event) {
-    event.preventDefault()
+var post = {}
 
-    var email = event.target.email.value
-    var password = event.target.password.value
-    
+post.email = users[0].email
+post.text = 'hello world'
+post.picture = 'https://b00tc4mp.com/discord.png'
+post.date = new Date
+
+posts.push(post)
+
+var post = {}
+
+post.email = users[1].email
+post.text = 'hallo welt'
+post.picture = 'https://www.hallo-welt.io/wp-content/uploads/2021/05/HalloWelt-LOGO-1transparent-1.png'
+post.date = new Date
+
+posts.push(post)
+
+// business (logic)
+
+function authenticateUser(email, password) {
     var found
 
     for (var i = 0; i < users.length; i++) {
@@ -48,7 +63,51 @@ document.querySelector('.login-page').querySelector('form').onsubmit = function 
         }
     }
 
-    if (found) {
+    if (found)
+        return true
+    else
+        return false
+}
+
+function registerUser(name, email, password) {
+    var found
+
+    for (var i = 0; i < users.length; i++) {
+        var user = users[i]
+
+        if (user.email === email) {
+            found = user
+
+            break
+        }
+    }
+
+    if (found)
+        return false
+    else {
+        var user = {}
+
+        user.name = name
+        user.email = email
+        user.password = password
+
+        users.push(user)
+
+        return true
+    }
+}
+
+// presentation
+
+document.querySelector('.login-page').querySelector('form').onsubmit = function (event) {
+    event.preventDefault()
+
+    var email = event.target.email.value
+    var password = event.target.password.value
+
+    var authenticated = authenticateUser(email, password)
+ 
+    if (authenticated) {
         document.querySelector('.login-page').classList.add('off')
         document.querySelector('.home-page').classList.remove('off')
     } else alert('Wrong credentials')
@@ -76,16 +135,33 @@ document.querySelector('.register-page').querySelector('form').onsubmit = functi
     var email = event.target.email.value
     var password = event.target.password.value
 
-    var user = {}
+    var registered = registerUser(name, email, password)
 
-    user.name = name
-    user.email = email
-    user.password = password
+    if (!registered)
+        alert('user already exists')
+    else {
+        alert('User registered')
 
-    users.push(user)
+        document.querySelector('.register-page').classList.add('off')
+        document.querySelector('.login-page').classList.remove('off')
+    }
+}
 
-    alert('User registered')
+document.querySelector('.home-page').querySelector('.create-post-button').onclick = function() {
+    document.querySelector('.home-page').querySelector('.post-modal').classList.remove('off')
+}
 
-    document.querySelector('.register-page').classList.add('off')
-    document.querySelector('.login-page').classList.remove('off')
+document.querySelector('.home-page').querySelector('.post-modal').querySelector('.cancel-button').onclick = function(event) {
+    event.preventDefault()
+
+    document.querySelector('.home-page').querySelector('.post-modal').classList.add('off')
+}
+
+document.querySelector('.home-page').querySelector('.post-modal').querySelector('.post-form').onsubmit = function(event) {
+    event.preventDefault()
+
+    var picture = event.target.picture.value
+    var text = event.target.text.value
+
+    console.log('TODO create a post', picture, text)
 }
