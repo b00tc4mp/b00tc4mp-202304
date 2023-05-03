01092a5dffac51070e6c20078dc5557b8973a2a9
Manuel Barzi <manuelbarzi@MacBook-Pro-de-Administrador.local>
2023-05-03 15:41:52 +0200

    add more arrays challenges

--- staff/manuel-barzi/arrays/join.js
+++ staff/manuel-barzi/arrays/join.js
@@ -1,5 +1,51 @@
+/*
 function join(array) {
-    // TODO implement me   
+    let joined = ''
+
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i]
+
+        joined = joined + element
+
+        if (i < array.length - 1)
+            joined = joined + ','
+    }
+
+    return joined
+}
+*/
+
+/*
+function join(array, separator = ',') {
+    let joined = ''
+
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i]
+
+        joined = joined + element
+
+        if (i < array.length - 1)
+            joined = joined + separator
+    }
+
+    return joined
+}
+*/
+
+function join(array, separator = ',') {
+    let joined = ''
+
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i]
+
+        if (element !== undefined)
+            joined = joined + element
+
+        if (i < array.length - 1)
+            joined = joined + separator
+    }
+
+    return joined
 }
 
 // tests
@@ -8,19 +54,33 @@ function join(array) {
 
 var elements = ['Fire', 'Air', 'Water']
 
-console.log(elements.join())
+console.log(join(elements))
 // "Fire,Air,Water"
 
 // case 2
 
 var elements = ['Fire', 'Air', 'Water']
 
-console.log(elements.join(''))
+console.log(join(elements, ''))
 // "FireAirWater"
 
 // case 3
 
 var elements = ['Fire', 'Air', 'Water']
 
-console.log(elements.join('$'))
+console.log(join(elements, '$'))
 // "Fire$Air$Water"
+
+// case 4
+
+var elements = [10, 20, 30]
+
+console.log(join(elements, '<'))
+// "10<20<30"
+
+// case 5
+
+var elements = [1, undefined, 3]
+    
+console.log(join(elements))
+// '1,,3'
